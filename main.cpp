d6e63edd7deb985e1dbf2a46c07e7055861e3593
janus_wel <janus.wel.3@gmail.com>
2015-06-18 00:39:42 +0900

    use sub function in main

--- main.cpp
+++ main.cpp
@@ -5,10 +5,14 @@
 #include <iostream>
 
 #include "add.hpp"
+#include "sub.hpp"
 
 int main(const int, const char* const []) {
     std::cout
         << add(1, 2)
+        << std::endl;
+    std::cout
+        << sub(1, 2)
         << std::endl;
 
     return 0;
